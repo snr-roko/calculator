1ea54418bf8d60c4de92812288c4eb0031bd8391
mr-r <rbbagyei@gmail.com>
2024-05-30 21:48:25 +0000

    Four functions of the numeric operations have been created. Functions for addition, subtraction, multiplication, and division operations have been created. Each has two arguments val1 and val2. This has been tested in the console.

---
+++ app.js
@@ -0,0 +1,21 @@
+// The four numeric operations functions
+
+// Addition
+function addition(val1, val2) {
+  return val1 + val2;
+}
+
+// Subtraction
+function subtraction(val1, val2) {
+  return val1 - val2;
+}
+
+// Multiplication
+function multiplication(val1, val2) {
+  return val1 * val2;
+}
+
+// Division
+function division(val1, val2) {
+  return val1 / val2;
+}
