15c1c25913b47f719334ff1477f8aaa77cb34915
mr-r <rbbagyei@gmail.com>
2024-05-30 22:06:18 +0000

    numeric operations function names changed, the three elements of an operation also created, and an operaton function has also been created The four numeric operations function names have been changed from nouns to verbs to conform to naming standards. The three elements of a numeric operation; two numbers and an operator has been initialized as variables. Also a function to determine what operation function to call depending on the operator has been created as a function using a swicth statement to configure the conditions.

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
 // The four numeric operations functions
 
 // Addition
-function addition(val1, val2) {
+function add(val1, val2) {
   return val1 + val2;
 }
 
 // Subtraction
-function subtraction(val1, val2) {
+function subtract(val1, val2) {
   return val1 - val2;
 }
 
 // Multiplication
-function multiplication(val1, val2) {
+function multiply(val1, val2) {
   return val1 * val2;
 }
 
 // Division
-function division(val1, val2) {
+function divide(val1, val2) {
   return val1 / val2;
+}
+
+// The three elements of a numeric operation
+let first_value;
+let second_value;
+let operator;
+
+// The operation function 
+function operate() {
+  let result;
+  switch(operator) {
+    case "+":
+      result = add(first_value, second_value);
+      break;
+    case "-":
+      result = subtract(first_value, second_value);
+      break;
+    case "*":
+      result = multiply(first_value, second_value);
+      break;
+    case "/":
+      result = divide(first_value, second_value);
+      break;
+  }
+  return result;
 }
